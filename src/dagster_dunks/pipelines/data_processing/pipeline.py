04461bdc0c5500227718e90d9140d56506842f41
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-16 11:57:02 -0600

    Add node to concat tourney results, reformat files

--- src/dagster_dunks/pipelines/data_processing/pipeline.py
+++ src/dagster_dunks/pipelines/data_processing/pipeline.py
@@ -3,8 +3,20 @@ This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.19.11
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+import ibis
+from kedro.pipeline import Pipeline, node, pipeline  # noqa
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=ibis.union,
+                inputs=[
+                    "mncaa_tourney_detailed_results",
+                    "wncaa_tourney_detailed_results",
+                ],
+                outputs="tourney_results",
+            )
+        ]
+    )
