22e29638bcf782618d549fad4f238c6251b2b394
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-18 12:48:31 -0600

    Make out-of-fold predictions based for evert split

--- src/dagster_dunks/pipelines/model_training/nodes.py
+++ src/dagster_dunks/pipelines/model_training/nodes.py
@@ -18,8 +18,8 @@ def find_iteration_counts(model_input_table, params):
 
     logger = logging.getLogger(__name__)
 
-    X = model_input_table.drop("y")
-    y = model_input_table.y
+    X = model_input_table.drop("y").execute().to_numpy()
+    y = model_input_table.y.execute().to_numpy()
     dtrain = xgb.DMatrix(X, label=y)
 
     def cauchyobj(preds, dtrain):
@@ -51,3 +51,36 @@ def find_iteration_counts(model_input_table, params):
     val_mae = [np.min(x["test-mae-mean"].values) for x in xgb_cv]
     logger.info("%s", (iteration_counts, val_mae))
     return iteration_counts
+
+
+def make_out_of_fold_predictions(model_input_table, iteration_counts, params):
+    """
+    This function takes the model input table and the number of iterations
+    for each fold and returns the out-of-fold predictions.
+    """
+
+    logger = logging.getLogger(__name__)
+
+    X = model_input_table.drop("y").execute().to_numpy()
+    y = model_input_table.y.execute().to_numpy()
+
+    repeat_cv = 3  # recommend 10
+
+    oof_preds = []
+    for i in range(repeat_cv):
+        logger.info("Fold repeater %d", i)
+        preds = y.copy()
+        kfold = KFold(n_splits=5, shuffle=True, random_state=i)
+        for train_index, val_index in kfold.split(X, y):
+            dtrain_i = xgb.DMatrix(X[train_index], label=y[train_index])
+            dval_i = xgb.DMatrix(X[val_index], label=y[val_index])
+            model = xgb.train(
+                params=params,
+                dtrain=dtrain_i,
+                num_boost_round=iteration_counts[i],
+                verbose_eval=50,
+            )
+            preds[val_index] = model.predict(dval_i)
+        oof_preds.append(np.clip(preds, -30, 30))
+
+    return oof_preds
--- src/dagster_dunks/pipelines/model_training/pipeline.py
+++ src/dagster_dunks/pipelines/model_training/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.11
 
 from kedro.pipeline import Pipeline, node, pipeline  # noqa
 
-from .nodes import find_iteration_counts
+from .nodes import find_iteration_counts, make_out_of_fold_predictions
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -15,6 +15,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=find_iteration_counts,
                 inputs=["model_input_table", "params:xgb_cv"],
                 outputs="iteration_counts",
+            ),
+            node(
+                func=make_out_of_fold_predictions,
+                inputs=["model_input_table", "iteration_counts", "params:xgb_cv"],
+                outputs="out_of_fold_predictions",
             ),
         ]
     )
