9a8e75b45233340e6aa2af2f5d88f494b95aab4d
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-18 00:41:00 -0600

    Compute regular season box score averages per team

--- src/dagster_dunks/pipelines/feature_engineering/nodes.py
+++ src/dagster_dunks/pipelines/feature_engineering/nodes.py
@@ -2,3 +2,15 @@
 This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.11
 """
+
+from ibis import _
+
+
+def calculate_season_stats(results_by_team, box_score_cols: list[str]):
+    """Calculate season averages for each team."""
+    return results_by_team.group_by(["Season", "TeamID"]).agg(
+        {
+            f"avg_{col}": _[col].mean()
+            for col in (*box_score_cols, *(f"opponent_{col}" for col in box_score_cols))
+        }
+    )
--- src/dagster_dunks/pipelines/feature_engineering/pipeline.py
+++ src/dagster_dunks/pipelines/feature_engineering/pipeline.py
@@ -3,8 +3,18 @@ This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.11
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro.pipeline import Pipeline, node, pipeline  # noqa
+
+from .nodes import calculate_season_stats
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                calculate_season_stats,
+                inputs=["regular_season_results_by_team", "params:box_score_cols"],
+                outputs="regular_season_averages",
+            )
+        ]
+    )
