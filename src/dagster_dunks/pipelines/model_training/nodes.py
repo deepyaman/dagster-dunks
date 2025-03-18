d10895c1dd60a63aca3c403e7e4d58501f387697
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-18 12:29:27 -0600

    Install XGBoost and calculate the iteration counts

--- src/dagster_dunks/pipelines/model_training/nodes.py
+++ src/dagster_dunks/pipelines/model_training/nodes.py
@@ -2,3 +2,52 @@
 This is a boilerplate pipeline 'model_training'
 generated using Kedro 0.19.11
 """
+
+import logging
+
+import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import KFold
+
+
+def find_iteration_counts(model_input_table, params):
+    """
+    This function takes the model input table and the cross-validation
+    parameters and returns the number of iterations for each fold.
+    """
+
+    logger = logging.getLogger(__name__)
+
+    X = model_input_table.drop("y")
+    y = model_input_table.y
+    dtrain = xgb.DMatrix(X, label=y)
+
+    def cauchyobj(preds, dtrain):
+        labels = dtrain.get_label()
+        c = 5000
+        x = preds - labels
+        grad = x / (x**2 / c**2 + 1)
+        hess = -(c**2) * (x**2 - c**2) / (x**2 + c**2) ** 2
+        return grad, hess
+
+    xgb_cv = []
+    repeat_cv = 3  # recommend 10
+
+    for i in range(repeat_cv):
+        logger.info("Fold repeater %d", i)
+        xgb_cv.append(
+            xgb.cv(
+                params=params,
+                dtrain=dtrain,
+                obj=cauchyobj,
+                num_boost_round=3000,
+                folds=KFold(n_splits=5, shuffle=True, random_state=i),
+                early_stopping_rounds=25,
+                verbose_eval=50,
+            )
+        )
+
+    iteration_counts = [np.argmin(x["test-mae-mean"].values) for x in xgb_cv]
+    val_mae = [np.min(x["test-mae-mean"].values) for x in xgb_cv]
+    logger.info("%s", (iteration_counts, val_mae))
+    return iteration_counts
--- src/dagster_dunks/pipelines/model_training/pipeline.py
+++ src/dagster_dunks/pipelines/model_training/pipeline.py
@@ -3,8 +3,18 @@ This is a boilerplate pipeline 'model_training'
 generated using Kedro 0.19.11
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro.pipeline import Pipeline, node, pipeline  # noqa
+
+from .nodes import find_iteration_counts
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=find_iteration_counts,
+                inputs=["model_input_table", "params:xgb_cv"],
+                outputs="iteration_counts",
+            ),
+        ]
+    )
