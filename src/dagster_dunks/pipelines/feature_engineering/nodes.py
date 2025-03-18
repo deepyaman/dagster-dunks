53f1a7ac0cf51569931cd8e5692dbc43b6dcff10
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-18 02:34:49 -0600

    Add regular season statistics to NCAA tourney data

--- src/dagster_dunks/pipelines/feature_engineering/nodes.py
+++ src/dagster_dunks/pipelines/feature_engineering/nodes.py
@@ -6,11 +6,21 @@ generated using Kedro 0.19.11
 from ibis import _
 
 
-def calculate_season_stats(results_by_team, box_score_cols: list[str]):
+def calculate_season_stats(regular_season_results_by_team, box_score_cols: list[str]):
     """Calculate season averages for each team."""
-    return results_by_team.group_by(["Season", "TeamID"]).agg(
+    return regular_season_results_by_team.group_by(["Season", "TeamID"]).agg(
         {
             f"avg_{col}": _[col].mean()
             for col in (*box_score_cols, *(f"opponent_{col}" for col in box_score_cols))
         }
+    )
+
+
+def join_season_stats(
+    ncaa_tourney_results_by_team, season_stats, box_score_cols: list[str]
+):
+    """Join season stats and opposing team's season stats to results."""
+    return ncaa_tourney_results_by_team.join(season_stats, ("Season", "TeamID")).join(
+        season_stats.rename("opponent_{name}").rename(Season="opponent_Season"),
+        ("Season", "opponent_TeamID"),
     )
--- src/dagster_dunks/pipelines/feature_engineering/pipeline.py
+++ src/dagster_dunks/pipelines/feature_engineering/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.19.11
 
 from kedro.pipeline import Pipeline, node, pipeline  # noqa
 
-from .nodes import calculate_season_stats
+from .nodes import calculate_season_stats, join_season_stats
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -15,6 +15,15 @@ def create_pipeline(**kwargs) -> Pipeline:
                 calculate_season_stats,
                 inputs=["regular_season_results_by_team", "params:box_score_cols"],
                 outputs="regular_season_averages",
-            )
+            ),
+            node(
+                join_season_stats,
+                inputs=[
+                    "ncaa_tourney_results_by_team",
+                    "regular_season_averages",
+                    "params:box_score_cols",
+                ],
+                outputs="ncaa_tourney_results_with_regular_season_averages",
+            ),
         ]
     )
