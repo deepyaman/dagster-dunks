6e7acbc0b19c8659f012ed12b882b9aaa49aa42e
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-03-18 16:59:36 -0600

    Generate predictions and write to `submission.csv`

--- src/dagster_dunks/pipelines/model_inference/nodes.py
+++ src/dagster_dunks/pipelines/model_inference/nodes.py
@@ -2,3 +2,87 @@
 This is a boilerplate pipeline 'model_inference'
 generated using Kedro 0.19.11
 """
+
+import logging
+
+import ibis
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from ibis import _
+
+
+def extract_unit_of_analysis_columns(sample_submission):
+    return sample_submission.mutate(
+        Season=_.ID.substr(0, 4).cast(int),
+        TeamID=_.ID.substr(5, 4).cast(int),
+        opponent_TeamID=_.ID.substr(10, 4).cast(int),
+        location=ibis.literal("N"),
+    )
+
+
+def make_predictions(
+    inference_model_input_table,
+    model_input_table,
+    iteration_counts,
+    spline_model,
+    params,
+):
+    logger = logging.getLogger(__name__)
+
+    sub = inference_model_input_table.execute()
+
+    Xsub = sub.drop(
+        columns=["Season", "TeamID", "opponent_TeamID", "location"]
+    ).to_numpy()
+    dtest = xgb.DMatrix(Xsub)
+
+    X = (
+        model_input_table.copy()
+        .drop(columns=["Season", "TeamID", "opponent_TeamID", "y"])
+        .to_numpy()
+    )
+    y = model_input_table.copy()["y"]
+    dtrain = xgb.DMatrix(X, label=y)
+
+    def cauchyobj(preds, dtrain):
+        labels = dtrain.get_label()
+        c = 5000
+        x = preds - labels
+        grad = x / (x**2 / c**2 + 1)
+        hess = -(c**2) * (x**2 - c**2) / (x**2 + c**2) ** 2
+        return grad, hess
+
+    repeat_cv = 3  # recommend 10
+
+    sub_models = []
+    for i in range(repeat_cv):
+        logger.info("Fold repeater %d", i)
+        sub_models.append(
+            xgb.train(
+                params=params,
+                dtrain=dtrain,
+                num_boost_round=int(iteration_counts[i] * 1.05),
+                verbose_eval=50,
+            )
+        )
+
+    sub_preds = []
+    for i in range(repeat_cv):
+        sub_preds.append(
+            np.clip(
+                spline_model[i](np.clip(sub_models[i].predict(dtest), -30, 30)),
+                0.025,
+                0.975,
+            )
+        )
+
+    sub["ID"] = (
+        sub["Season"].astype(str)
+        + "_"
+        + sub["TeamID"].astype(str)
+        + "_"
+        + sub["opponent_TeamID"].astype(str)
+    )
+    sub["Pred"] = pd.DataFrame(sub_preds).mean(axis=0)
+    return sub[["ID", "Pred"]]
--- src/dagster_dunks/pipelines/model_inference/pipeline.py
+++ src/dagster_dunks/pipelines/model_inference/pipeline.py
@@ -3,8 +3,92 @@ This is a boilerplate pipeline 'model_inference'
 generated using Kedro 0.19.11
 """
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro.pipeline import Pipeline, node, pipeline  # noqa
+
+from dagster_dunks.pipelines.feature_engineering.nodes import (
+    join_season_stats,
+    join_win_ratios,
+    join_team_quality_scores,
+    join_seeds,
+    encode_location,
+    calculate_avg_score_diff,
+    calculate_seed_diff,
+    create_model_input_table,
+)
+from .nodes import (
+    extract_unit_of_analysis_columns,
+    make_predictions,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=extract_unit_of_analysis_columns,
+                inputs=["sample_submission"],
+                outputs="sample_submission_with_unit_of_analysis_columns",
+            ),
+            node(
+                func=join_season_stats,
+                inputs=[
+                    "sample_submission_with_unit_of_analysis_columns",
+                    "regular_season_averages",
+                    "params:box_score_cols",
+                ],
+                outputs="sample_submission_with_season_stats",
+            ),
+            node(
+                func=join_win_ratios,
+                inputs=["sample_submission_with_season_stats", "win_ratios"],
+                outputs="sample_submission_with_win_ratios",
+            ),
+            node(
+                func=join_team_quality_scores,
+                inputs=["sample_submission_with_win_ratios", "team_quality_scores"],
+                outputs="sample_submission_with_team_quality_scores",
+            ),
+            node(
+                func=join_seeds,
+                inputs=["sample_submission_with_team_quality_scores", "seeds"],
+                outputs="inference_master_table",
+            ),
+            node(
+                func=encode_location,
+                inputs="inference_master_table",
+                outputs="inference_encoded_location",
+            ),
+            node(
+                func=calculate_avg_score_diff,
+                inputs=["inference_master_table"],
+                outputs="inference_avg_score_diff",
+            ),
+            node(
+                func=calculate_seed_diff,
+                inputs=["inference_master_table"],
+                outputs="inference_seed_diff",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=[
+                    "inference_master_table",
+                    "sample_submission",  # hack
+                    "inference_encoded_location",
+                    "inference_avg_score_diff",
+                    "inference_seed_diff",
+                ],
+                outputs="inference_model_input_table",
+            ),
+            node(
+                func=make_predictions,
+                inputs=[
+                    "inference_model_input_table",
+                    "model_input_table_pandas",
+                    "iteration_counts",
+                    "spline_model",
+                    "params:xgb_cv",
+                ],
+                outputs="submission",
+            ),
+        ]
+    )
